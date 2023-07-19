f06b7ae3c85b0ef6344eb1b7eade0496677d1306
Adithya Rajesh Pillai <82641329+adithya16pillai@users.noreply.github.com>
2023-07-20 02:34:11 +0530

    Update checksum.cpp

--- Computer Networks/checksum.cpp
+++ Computer Networks/checksum.cpp
@@ -1,23 +1,39 @@
-#include <stdio.h>
-
-unsigned short calculateChecksum(const int *data, int length) {
-    unsigned int sum = 0;
-    int i;
-    for (i = 0; i < length; i++) {
-        sum += data[i];
-    }
-    while (sum >> 16) {
-        sum = (sum & 0xFFFF) + (sum >> 16);
-    }
-    return (unsigned short)(~sum);
+#include<stdio.h>
+#include<math.h>
+ 
+int sender(int arr[10],int n) {
+    int checksum,sum=0,i;
+    printf("\nSender Side\n");
+    for(i=0;i<n;i++)
+    sum+=arr[i];
+    printf("Sum is: %d",sum);
+    checksum=~sum;   
+    printf("\Checksum is:%d",checksum);
+    return checksum;
 }
-
-int main() {
-    int data[10];
-    printf("Enter data: ");
-    scanf("%d", &data);
-    int length = sizeof(data) - 1;
-    unsigned short checksum = calculateChecksum(data, length);
-    printf("Checksum: 0x%04X\n", checksum);
-    return 0;
+ 
+void receiver(int arr[10],int n,int sch) {
+    int checksum,sum=0,i;
+    printf("\n\nReciever Side\n");
+    for(i=0;i<n;i++)
+        sum+=arr[i];
+    printf("Sum is:%d",sum);
+    sum=sum+sch;
+    checksum=~sum; 
+    printf("\Checksum is:%d",checksum);
+}
+ 
+void main() {
+    int n,sch,rch;
+    printf("\nEnter size of the string:");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the elements of the array to calculate the checksum:\n");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    sch=sender(arr,n);
+    receiver(arr,n,sch);
+    printf("21BCE3546");
 }
