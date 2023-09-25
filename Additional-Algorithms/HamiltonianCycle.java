a372877b545e83b2313973d179635f24fc7056c3
Adithya Rajesh Pillai <82641329+adithya16pillai@users.noreply.github.com>
2023-09-26 00:40:27 +0530

    Hamiltonian Cycle

---
+++ Additional-Algorithms/HamiltonianCycle.java
@@ -0,0 +1,39 @@
+public class HamiltonianCycle {
+    final int V = 5;
+    int path[];
+    boolean isSafe(int v, int graph[][], int path[], int pos){
+        if (graph[path[pos - 1]][v] == 0)
+            return false;
+        for (int i = 0; i < pos; i++)
+            if (path[i] == v)
+                return false;
+        return true;
+    }
+    boolean hamCycleUtil(int graph[][], int path[], int pos){
+        if (pos == V){
+            if (graph[path[pos - 1]][path[0]] == 1)
+                return true;
+            else
+                return false;} 
+        for (int v = 1; v < V; v++){
+            if (isSafe(v, graph, path, pos)){
+                path[pos] = v;
+                if (hamCycleUtil(graph, path, pos + 1) == true)
+                    return true;
+                path[pos] = -1;
+                } 
+            }
+            return false; 
+        }
+        {
+        path = new int[V];
+        for (int i = 0; i < V; i++)
+        path[i] = -1;
+        path[0] = 0;
+        if (hamCycleUtil(graph, path, 1) == false){
+            System.out.println("\nSolution does not exist");
+            return 0;
+        }
+        printSolution(path);
+        return 1;} 
+}
